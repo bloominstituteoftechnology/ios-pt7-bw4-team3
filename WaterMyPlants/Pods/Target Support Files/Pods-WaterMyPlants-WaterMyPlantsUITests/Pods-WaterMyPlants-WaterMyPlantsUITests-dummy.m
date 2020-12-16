c89e3f650152d5ace0ebdc4639c93bdaaedb0566
Craig Belinfante <craigbasketball@aol.com>
2020-12-15 21:04:21 -0800

    deleted reference project. Created new project called Planty

--- WaterMyPlants/Pods/Target Support Files/Pods-WaterMyPlants-WaterMyPlantsUITests/Pods-WaterMyPlants-WaterMyPlantsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WaterMyPlants_WaterMyPlantsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WaterMyPlants_WaterMyPlantsUITests
-@end
