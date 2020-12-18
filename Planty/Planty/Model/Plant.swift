79f8de108fc4e3dbce69a4fd1096e5c3428165d1
craig <craigbasketball@aol.com>
2020-12-17 19:49:18 -0800

    created plant model

---
+++ Planty/Planty/Model/Plant.swift
@@ -0,0 +1,16 @@
+//
+//  Plant.swift
+//  Planty
+//
+//  Created by Craig Belinfante on 12/17/20.
+//
+
+import Foundation
+
+struct Plant {
+    var name: String
+    var plantType: String
+    var waterDate: Date
+    var plantImage: URL?
+    var audioNotes: URL?
+}
